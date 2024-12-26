d76ae92d98886504cb4973b9ae28b10e5c17a61f
chandu078 <chandudyavanapelli03@gmail.com>
2024-12-26 12:44:26 +0530

    Pong: Initial import from NOS 3.0
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,28 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    multiimgoem \
+    multiimgqti \
+    qupfw \
+    qweslicstore \
+    shrm \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
